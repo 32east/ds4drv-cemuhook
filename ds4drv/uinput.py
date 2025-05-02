887171d8ed551a80b9c19b80685b3aff2321010c
32east <tvrar30@gmail.com>
2025-05-02 20:43:50 +0600

    Update uinput.py

--- ds4drv/uinput.py
+++ ds4drv/uinput.py
@@ -223,7 +223,7 @@ create_mapping(
 create_mapping(
     "mouse", "DualShock4 Mouse Emulation",
     buttons={
-        "BTN_LEFT": "button_trackpad",
+        "BTN_TOUCH": "button_trackpad",
     },
     mouse={
         "REL_X": "trackpad_touch0_x",
